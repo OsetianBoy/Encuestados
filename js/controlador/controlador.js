4056f3f3f9c9b3bc6a24d33ac2c5eb9c646b63b0
OsetianBoy <ebaskaev78@hotmail.com>
2019-12-03 15:43:57 -0300

    initial commit

---
+++ js/controlador/controlador.js
@@ -0,0 +1,36 @@
+/*
+ * Controlador
+ */
+var Controlador = function(modelo) {
+  this.modelo = modelo;
+};
+
+Controlador.prototype = {
+  agregarPregunta: function(pregunta, respuestas) {
+      this.modelo.agregarPregunta(pregunta, respuestas);
+  },
+
+  botonBorrarPregunta: function(){
+    var id = parseInt($('.list-group-item.active').attr('id'));
+    if (id > 0){
+      this.modelo.borrarPregunta(id);
+    }
+  },
+
+  botonEditarPregunta: function(nuevoNombre){
+    var id = parseInt($('.list-group-item.active').attr('id'));
+    if(id > 0){
+      this.modelo.editarPregunta(id, nuevoNombre);
+    }
+  },
+
+  borrarTodo: function(){
+   this.modelo.borrarTodo();
+  },
+
+  agregarVoto: function(pregunta,respuestaSeleccionada){
+    this.modelo.agregarVoto(pregunta, respuestaSeleccionada);
+  },
+};
+
+
---
+++ js/modelo/modelo.js
@@ -0,0 +1,98 @@
+/*
+ * Modelo
+ */
+var Modelo = function() {
+  this.preguntas = [];
+  this.ultimoId = 0;
+
+  //inicializacion de eventos
+  this.preguntaAgregada = new Evento(this);
+  this.preguntaEliminada = new Evento(this);
+  this.preguntaEditada = new Evento(this);
+  this.preguntasBorradas = new Evento(this);
+  this.votoAgregado = new Evento(this);
+  this.verificarLocalStorage();
+};
+
+
+Modelo.prototype = {
+  //se obtiene el id más grande asignado a una pregunta
+  obtenerUltimoId: function() {
+    if (this.preguntas.length === 0){
+      return 0;
+    }
+    /*} else {
+      return this.preguntas[this.preguntas.length-1].id;
+    }*/
+    var idMasAlto = 0;
+    for(var i= 0; i<this.preguntas.length; i++){
+      if(this.preguntas[i].id> idMasAlto){
+        idMasAlto = this.preguntas[i].id;
+      }
+    }
+    return idMasAlto;
+  },
+
+  //Guia 2 paso 1 - Funcionalidades del modelo
+  //se agrega una pregunta dado un nombre y sus respuestas
+  agregarPregunta: function(nombre, respuestas) {
+    var id = this.obtenerUltimoId();
+    id++;
+    var nuevaPregunta = {'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas};
+    this.preguntas.push(nuevaPregunta);
+    this.guardar();
+    this.preguntaAgregada.notificar();
+  },
+
+  borrarPregunta: function(id){
+    this.preguntas = this.preguntas.filter(function(elemento){
+       return elemento.id != id; 
+    });
+    this.guardar();
+    this.preguntaEliminada.notificar();
+  },
+
+  editarPregunta: function(id, nuevoTexto){
+    this.preguntas.forEach(function(pregunta){
+      if (pregunta.id == id) {
+        pregunta.textoPregunta = nuevoTexto;
+      }
+    });
+    this.guardar();
+    this.preguntaEditada.notificar();
+  },
+
+
+  borrarTodo: function(){
+    this.preguntas = [];
+    this.guardar();
+    this.preguntasBorradas.notificar();
+  },
+
+  //Se agregan votos
+  agregarVoto: function(id, respuesta){
+    this.preguntas.forEach(function(pregunta){
+      if(pregunta.id == id){
+        pregunta.cantidadPorRespuesta.forEach(function(r){
+          if(r.textoRespuesta == respuesta){
+            r.cantidad += 1;
+          };
+        });
+      };
+    });
+    this.guardar();
+    this.votoAgregado.notificar();
+  },
+
+
+//Guia 2 paso 2 - agrego LocalStorage
+  guardar: function () {
+    localStorage.setItem('preguntas', JSON.stringify(this.preguntas));
+  },
+  verificarLocalStorage: function () {
+    if (localStorage.getItem('preguntas') !== null) {
+      this.preguntas = JSON.parse(localStorage.getItem('preguntas'));
+    }
+  },
+};
+
